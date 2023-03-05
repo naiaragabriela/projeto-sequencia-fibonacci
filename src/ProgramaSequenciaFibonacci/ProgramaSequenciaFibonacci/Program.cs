472973e6ac6a3264a160ce316e48de7ef1950212
naiaragabriela <naigabi31@gmail.com>
2023-03-05 18:34:32 -0300

    sequencia fibonacci

---
+++ src/ProgramaSequenciaFibonacci/ProgramaSequenciaFibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int anterior, proximo, atual, posicao;
+
+SequenciaFibonacci();
+
+int BuscaPosicao()
+{
+    Console.Write("Informe até qual posição da sequência você deseja ver: ");
+    return int.Parse(Console.ReadLine()); 
+}
+
+void SequenciaFibonacci()
+{
+    posicao = BuscaPosicao();
+    anterior = 0;
+    proximo = 1;
+    atual = 1;
+
+    for (int i = 1; i <= posicao; i++)
+    {
+        Console.Write(atual + ",");
+        atual = proximo + anterior;
+        anterior = proximo;
+        proximo = atual;
+    }
+}
